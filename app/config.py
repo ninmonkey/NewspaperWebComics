c509aa4db32b325f629548e9d9d889819f3eed2a
Jake Bolton <ninmonkeys@gmail.com>
2018-05-09 13:22:21 -0500

    loads config

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ def load_config():
     global config
 
     path = os.path.join('config', 'config.json')
+    os.makedirs(path, exist_ok=True)
     with open(path) as f:
         config = json.load(f)
 
